3233adca69d70cc0550a94cd9c513cceef1a5e73
Dragonyong-siu <68500343+Dragonyong-siu@users.noreply.github.com>
2021-01-25 00:34:47 +0900

    Create adjust_lr.py

---
+++ utils/adjust_lr.py
@@ -0,0 +1,6 @@
+# adjust_lr.py
+
+def adjust_lr(optimizer, shrink_factor):
+  for param_group in optimizer.param_groups:
+    param_group['lr'] = param_group['lr'] * shrink_factor
+  print(' adjust_LR is completed')
